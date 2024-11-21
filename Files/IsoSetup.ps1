d832e6a7ace4165c8e61a6de6f9b4244c64d96e9
Gianluca De Rossi <gderossi@microsoft.com>
2024-11-21 10:52:37 +0100

    Update IsoSetup.ps1 to use AzCopy for SQL Server ISO download and adjust command execution in azcli script

--- Files/IsoSetup.ps1
+++ Files/IsoSetup.ps1
@@ -32,13 +32,13 @@ $Headers = @{
 }
 
 Write-Host "Downloading SQL Server ISO..." -ForegroundColor Green
-# Download the SQL Server ISO
+# Download the SQL Server ISO via azcopy
 $ProgressPreference = 'SilentlyContinue'
-#Invoke-WebRequest -Uri "https://$StorageAccountName.blob.core.windows.net/$ContainerName/$BlobName" -Headers $Headers -Method Get -OutFile "D:\SQLServer2022-x64-ENU.iso"
 Invoke-WebRequest -Uri "https://aka.ms/downloadazcopy-v10-windows" -OutFile "D:\azcopy.zip"
 Expand-Archive -Path "D:\azcopy.zip" -DestinationPath "D:\"
 Copy-Item -Path "D:\azcopy_windows_amd64_10.27.1\azcopy.exe" -Destination "C:\Windows\System32\"
-azcopy "https://$StorageAccountName.blob.core.windows.net/$ContainerName/$BlobName" "D:\SQLServer2022-x64-ENU.iso"
+azcopy login --identity 
+azcopy copy "https://$StorageAccountName.blob.core.windows.net/$ContainerName/$BlobName" "D:\SQLServer2022-x64-ENU.iso"
 
 Write-Host "Copying SQL Server ISO content to C:\SQL2022..." -ForegroundColor Green
 # Copy the ISO content to C:\SQL2022
